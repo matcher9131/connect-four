de455c7b670ea0a7bbe2ceef1cf55821bb36f30d
matcher9131 <matcher9131cd@gmail.com>
2026-07-26 10:36:04 +0900

    feat #10: GUIアニメーションを実装

--- nextjs/src/app/_components/Board.tsx
+++ nextjs/src/app/_components/Board.tsx
@@ -1,30 +1,35 @@
-"use client"
+"use client";
 
 import { wasmPromise } from "@/lib/wasm";
 import { use, useState } from "react";
-import Column from "./Column";
 import { BOARD_SIZE } from "./const";
+import BackgroundLayer from "./BackgroundLayer";
+import PiecesLayer from "./PiecesLayer";
+import PlateLayer from "./PlateLayer";
+import ButtonLayer from "./ButtonLayer";
 
 type PutPieceReturnType = {
     readonly board: number[];
     readonly game_result: number;
 };
 
-const Board = () => {
+export default function Board() {
     const wasm = use(wasmPromise);
 
     const [pieces, setPieces] = useState(new Array(BOARD_SIZE * BOARD_SIZE).fill(0));
     const [isBlack, setIsBlack] = useState(false);
-    const [canColumnClick, setCanColumnClick] = useState(true);
     const [gameResult, setGameResult] = useState(0);
+    const [canColumnClick, setCanColumnClick] = useState(true);
 
-    const pieces2d = Array.from(
-        { length: BOARD_SIZE },
-        (_, colIndex) => pieces.slice(colIndex * BOARD_SIZE, (colIndex + 1) * BOARD_SIZE)
-    );
+    const [hoveredColIndex, setHoveredColIndex] = useState<number | null>(null);
+    const disabled = gameResult !== 0 || !canColumnClick;
 
-    const onColumnClick = (colIndex: number) => () => {
-        if (!canColumnClick) return;
+    const setHoveredColIndexFactory = (colIndex: number | null) => () => {
+        setHoveredColIndex(colIndex);
+    };
+
+    const handleColumnClick = (colIndex: number) => () => {
+        if (disabled) return;
         // 置けないときは何もしない
         if (pieces[(colIndex + 1) * BOARD_SIZE - 1] !== 0) return;
 
@@ -36,27 +41,27 @@ const Board = () => {
         if (result.game_result === 0) {
             setCanColumnClick(true);
         }
-
-        //
-        console.log(result.board);
-        console.log(result.game_result);
-        //
     };
-    
+
     return (
-        <div className="relative w-[80vh] h-[80vh]">
-            <div className="grid grid-cols-7 w-full h-full">
-                {pieces2d.map((pieces, colIndex) =>
-                    <Column
-                        key={colIndex}
-                        pieces={pieces}
-                        onClick={onColumnClick(colIndex)}
-                    />
-                )}
-            </div>
-            {gameResult !== 0 && <div className="absolute left-0 top-0 w-full h-full flex justify-center items-center text-3xl">{`${gameResult === 1 ? "先手" : "後手"}の勝ち`}</div>}
+        <div className="relative isolate aspect-square w-full max-w-[560px] select-none">
+            <BackgroundLayer />
+            <PiecesLayer
+                disabled={disabled}
+                pieces={pieces}
+                currentPlayer={isBlack ? 2 : 1}
+                hoveredColIndex={hoveredColIndex}
+            />
+            <PlateLayer />
+            <ButtonLayer
+                disabled={disabled}
+                pieces={pieces}
+                onHoveredColIndexChangeFactory={setHoveredColIndexFactory}
+                onColumnClickFactory={handleColumnClick}
+            />
+            {gameResult !== 0 && <div className="absolute z-40 left-0 top-0 w-full h-full flex justify-center items-center text-3xl">
+                <div className="bg-white/[0.5] p-2">{`${gameResult === 1 ? "先手" : "後手"}の勝ち`}</div>
+            </div>}
         </div>
     );
-};
-
-export default Board;
+}
