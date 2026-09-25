cdbcf866cb97409d82794dc580b58380e0b67d8a
matcher9131 <matcher9131cd@gmail.com>
2026-09-26 07:55:03 +0900

    本番デプロイ前の調整

--- nextjs/src/lib/wasm.ts
+++ nextjs/src/lib/wasm.ts
@@ -2,9 +2,11 @@
 
 type WasmModule = typeof import("../../../rust-wasm/pkg/connect_four_wasm");
 
+const wasmJsPath = "/wasm/connect_four_wasm.js";
+
 const load = async (): Promise<WasmModule> => {
     const mod = (await import(
-        /* webpackIgnore: true */ "/wasm/connect_four_wasm.js"
+        /* webpackIgnore: true */ wasmJsPath
     )) as unknown as WasmModule;
 
     await mod.default({ module_or_path: "/wasm/connect_four_wasm_bg.wasm" });
